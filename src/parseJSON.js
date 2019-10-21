7b9e8157e60ac80170b701bb9880afd47c3372e9
f3lixding <felixding1989@gmail.com>
2019-10-21 12:09:32 -0700

    Began parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,17 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json[0] === '[') {
+    var res = [];
+    var temp = json.substring(1, json.length - 1).split(', ');
+    for (var i = 0; i < temp.length; i++) {
+      res.push(parseJSON(temp[i]));
+    }
+  } else if (json[0] === '{') {
+    var res = {};
+    var temp = json.substring(1, json.length - 1);
+  } else if (json[0] === '"') {
+    var res = '';
+  }
+  return res;
 };
