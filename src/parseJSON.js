00ed95c0531be49c48b856123d25b53991d49b0c
f3lixding <felixding1989@gmail.com>
2019-10-21 20:10:14 -0700

    Refactors parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -15,44 +15,50 @@ var openBrackets = {
 
 var parseJSON = function(json) {
   // your code goes here
-  // if (json[0] === '[') {
-  //   var res = [];
-  //   var temp = json.substring(1, json.length - 1);
-  //   var tempString = '';
-  //   var brackets = [];
-  //   for (var i = 0; i < temp.length; i++) {
-  //     if (isOpen(temp[i]) || temp[i] === '"') {
-  //       if (brackets[brackets.length - 1] === '"') {
-  //         brackets.pop();
-  //       }
-  //       brackets.push(temp[i]);
-  //       tempString += temp[i];
-  //     } else if (temp[i] === ']' || temp[i] === '}') {
-  //       if (brackets[brackets.length - 1] === openBrackets[temp[i]]) {
-  //         brackets.pop();
-  //       }
-  //       tempString += temp[i];
-  //     } else if (temp[i] === ',' && !brackets.length) {
-  //       res.push(parseJSON(tempString));
-  //       tempString = '';
-  //     }
-  //   }
-  //   if (!!tempString) {
-  //     res.push(parseJSON(tempString));
-  //   }
-  //   return res;
-  // } else if (json[0] === '"') {
-  //   return json.substring(1, json.length -1 );
-  // }
-  var result = [];
   if (json[0] === '[') {
-    var temp = json.substring(1, json.length - 1).split(', ');
+    var res = [];
+    var temp = json.substring(1, json.length - 1);
+    var tempString = '';
+    var brackets = [];
     for (var i = 0; i < temp.length; i++) {
-      if (!!parseInt(temp[i])) {
-        result.push(parseInt(temp[i]));
+      if (isOpen(temp[i]) || temp[i] === '"') {
+        if (brackets[brackets.length - 1] === '"') {
+          brackets.pop();
+        } else {
+          brackets.push(temp[i]);
+        }
+        tempString += temp[i];
+      } else if (temp[i] === ']' || temp[i] === '}') {
+        if (brackets[brackets.length - 1] === openBrackets[temp[i]]) {
+          brackets.pop();
+        }
+        tempString += temp[i];
+      } else if (temp[i] === ',' && !brackets.length) {
+        res.push(parseJSON(tempString));
+        tempString = '';
+        if (i < temp.length - 1 && temp[i+1] === ' ') {
+          i++;
+        }
       } else {
-        result.push(temp[i]);
+        tempString += temp[i];
       }
     }
+    if (!!tempString) {
+      res.push(parseJSON(tempString));
+    }
+    return res;
+  } else if (json[0] === '"') {
+    return json.substring(1, json.length -1 );
   }
+  // if (json[0] === '[') {
+  // var result = [];
+  //   var temp = json.substring(1, json.length - 1).split(', ');
+  //   for (var i = 0; i < temp.length; i++) {
+  //     if (!!parseInt(temp[i])) {
+  //       result.push(parseInt(temp[i]));
+  //     } else {
+  //       result.push(temp[i]);
+  //  g   }
+  //   }
+  // }
 };
