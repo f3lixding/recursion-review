105db15a13157ee70de7357ef582573f4d75b81d
f3lixding <felixding1989@gmail.com>
2019-10-21 17:34:02 -0700

    Attempts parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,19 +2,57 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+var isOpen = function(bracket) {
+  if (bracket === '[' || bracket === '{') {
+    return true;
+  }
+};
+
+var openBrackets = {
+  ']' : '[',
+  '}' : '{',
+};
+
 var parseJSON = function(json) {
   // your code goes here
+  // if (json[0] === '[') {
+  //   var res = [];
+  //   var temp = json.substring(1, json.length - 1);
+  //   var tempString = '';
+  //   var brackets = [];
+  //   for (var i = 0; i < temp.length; i++) {
+  //     if (isOpen(temp[i]) || temp[i] === '"') {
+  //       if (brackets[brackets.length - 1] === '"') {
+  //         brackets.pop();
+  //       }
+  //       brackets.push(temp[i]);
+  //       tempString += temp[i];
+  //     } else if (temp[i] === ']' || temp[i] === '}') {
+  //       if (brackets[brackets.length - 1] === openBrackets[temp[i]]) {
+  //         brackets.pop();
+  //       }
+  //       tempString += temp[i];
+  //     } else if (temp[i] === ',' && !brackets.length) {
+  //       res.push(parseJSON(tempString));
+  //       tempString = '';
+  //     }
+  //   }
+  //   if (!!tempString) {
+  //     res.push(parseJSON(tempString));
+  //   }
+  //   return res;
+  // } else if (json[0] === '"') {
+  //   return json.substring(1, json.length -1 );
+  // }
+  var result = [];
   if (json[0] === '[') {
-    var res = [];
     var temp = json.substring(1, json.length - 1).split(', ');
     for (var i = 0; i < temp.length; i++) {
-      res.push(parseJSON(temp[i]));
+      if (!!parseInt(temp[i])) {
+        result.push(parseInt(temp[i]));
+      } else {
+        result.push(temp[i]);
+      }
     }
-  } else if (json[0] === '{') {
-    var res = {};
-    var temp = json.substring(1, json.length - 1);
-  } else if (json[0] === '"') {
-    var res = '';
   }
-  return res;
 };
